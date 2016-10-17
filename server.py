510f27b8266d0390d03fecc44da9bda614eb5c66
Jason Campbell <jrcampbell78@gmail.com>
2016-10-16 20:59:49 -0400

    Adds the ability to edit an existing page, saving a record of all changes in the history database.  CamelCase connversion to links, and markdown support added as well.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request
 import pg
+import markdown
 from wiki_linkify import wiki_linkify
 
 app =Flask('Wiki')
@@ -15,7 +16,9 @@ def render_page(page_name):
     if len(query.namedresult()) > 0:
         query = query.namedresult()[0]
         pages_id = query.id
-        newest_content = db.query("select content from history where pages_id = '%d' order by date_saved" %pages_id).namedresult()[0].content
+        newest_content = db.query("select content from history where pages_id = '%d' order by date_saved desc" %pages_id).namedresult()[0].content
+        newest_content = wiki_linkify(newest_content)
+        newest_content = markdown.markdown(newest_content)
         return render_template(
             'existing_page.html',
             page_name = page_name,
@@ -26,21 +29,36 @@ def render_page(page_name):
         'placeholder.html',
         page_name=page_name
         )
+
 @app.route('/<page_name>/edit')
 def render_edit_page(page_name):
-    return render_template(
-    'edit_page.html',
-    page_name=page_name,
-    )
+    query = db.query("select * from pages where title = '%s'" %page_name)
+    if len(query.namedresult()) == 0:
+        return render_template(
+        'edit_page.html',
+        page_name=page_name,
+        )
+    else:
+        query = query.namedresult()[0]
+        pages_id = query.id
+        newest_content = db.query("select content from history where pages_id = '%d' order by date_saved desc" %pages_id).namedresult()[0].content
+        return render_template(
+            'edit_page.html',
+            page_name = page_name,
+            newest_content = newest_content
+        )
 
 @app.route('/<page_name>/save', methods=['POST'])
 def submit_edit_page(page_name):
+    query = db.query("select * from pages where title = '%s'" %page_name)
+    if len(query.namedresult()) == 0:
+        db.insert(
+            'pages',
+            title = page_name
+        )
+    else:
+        pass
     content = request.form.get('edits')
-
-    db.insert(
-        'pages',
-        title = page_name
-    )
     query = db.query("select * from pages where title = '%s'" %page_name)
     query = query.namedresult()[0].id
     db.insert(
